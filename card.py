7cbd4e8457f92b1968ce399b2530d1dbfa8e889a
MariaKAP1 <121195659+MariaKAP1@users.noreply.github.com>
2023-02-21 20:06:21 +0000

    finished the test.

--- card.py
+++ card.py
@@ -1,6 +1,15 @@
-# Name:
+# Name: Maria Azconegui 
 
 class Card:
-    """class representing a simple Neapolitan card. Holds a 
-    value and a suit"""
+    def __init__(self, value:range(1,10), suit:str):
+        self.value = value
+        self.suit = suit
+    def __repr__(self): 
+        return f"Card(value = {self.value},suit = {self.suit})"
+    def __str__(self): 
+        return f"{self.value} of {self.suit}"
+    def __eq__(self):
+        return (self.value, self.suit) == (self.value, self.suit)
+
+
     
